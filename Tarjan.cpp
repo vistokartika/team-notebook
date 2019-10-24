376c0fb8331023dfc3741c20707de4a17469eb96
vistokartika <38373457+vistokartika@users.noreply.github.com>
2019-10-24 20:12:14 +0700

    Update Tarjan.cpp

--- Tarjan.cpp
+++ Tarjan.cpp
@@ -61,3 +61,61 @@ int main() {
 		}
 	}
 }
+
+// SCC
+int n,m,u,v,d,num[10005],low[10005],par[10005],child,root,pigeon[10005],nat;
+vector<int> adjlist[10005];
+
+void dfs(int now)
+{
+	num[now]=d;
+	low[now]=d;
+	d++;
+	pigeon[now]=1;
+	for(int i=0;i<adjlist[now].size();i++)
+	{
+		int next=adjlist[now][i];
+		if(num[next]==-1)
+		{
+			par[next]=now;
+			dfs(next);
+			if(par[now]==-1)
+			{
+				child++;
+			}
+			low[now]=min(low[now],low[next]);
+			if(low[next]>=num[now])
+			{
+				pigeon[now]++;
+			}
+		}
+		else if(next!=par[now])
+		{
+			low[now]=min(low[now],num[next]);
+		}
+	}
+	if(par[now]==-1)
+	{
+		pigeon[now]=child;
+	}
+}
+
+// pigeon[i] is scc of node i
+
+int main() {
+	cin >> n >> m;
+	d=0;
+	child=0;
+	memset(par,-1,sizeof(par));
+	memset(num,-1,sizeof(num));
+	memset(low,-1,sizeof(low));
+	for(int i = 0; i < m; i++) {
+		cin >> u >> v;
+		adjlist[u].push_back(v);
+		adjlist[v].push_back(u);
+	}
+	dfs(0);
+	for(int i = 0; i < n; i++) {
+		cout << i << " " << pigeon[i] << endl;
+	}
+}
