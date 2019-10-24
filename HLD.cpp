a89bba7f39ef5e3053b00ae3b0cbb64683597c59
Owmicron <55684780+Owmicron@users.noreply.github.com>
2019-10-24 17:01:29 +0700

    Update HLD.cpp

--- HLD.cpp
+++ HLD.cpp
@@ -3,39 +3,28 @@ vector<pair<int, int>> adjlist[MSIZE];
 int chainhead[MSIZE], chainidx[MSIZE], pos[MSIZE];
 int parent[MSIZE], basearr[MSIZE], subsize[MSIZE];
 int ptr, chainno;
-
 void hld(int cur, int cost, int prev){
-	if(chainhead[chainno] == -1){
-		chainhead[chainno] = cur;
-	}
+	if(chainhead[chainno] == -1) chainhead[chainno] = cur;
 	chainidx[cur] = chainno;
 	pos[cur] = ptr;
 	basearr[ptr] = cost;
 	ptr++;
-
 	int nextcost, nextchild = -1;
 	int sz = adjlist[cur].size();
 	for(int i = 0; i < sz; i++){
-		if(adjlist[cur][i].first == prev)
-			continue;
+		if(adjlist[cur][i].first == prev) continue;
 		if(nextchild == -1 || subsize[nextchild] < subsize[adjlist[cur][i].first]){
 			nextchild = adjlist[cur][i].first;
 			nextcost = adjlist[cur][i].second;
 		}
 	}
-
-	if(nextchild != -1){
-		hld(nextchild, nextcost, cur);
-	}
-
+	if(nextchild != -1) hld(nextchild, nextcost, cur);
 	for(int i = 0; i < sz; i++){
-		if(adjlist[cur][i].first == prev || adjlist[cur][i].first == nextchild)
-			continue;
+		if(adjlist[cur][i].first == prev || adjlist[cur][i].first == nextchild) continue;
 		chainno++;
 		hld(adjlist[cur][i].first, adjlist[cur][i].second, cur);
 	}
 }
-
 void dfs(int cur, int prev){
 	subsize[cur] = 1;
 	int sz = adjlist[cur].size();
@@ -47,7 +36,6 @@ void dfs(int cur, int prev){
 		subsize[cur] += subsize[adjlist[cur][i].first];
 	}
 }
-
 int query(int u, int v){
 	if(u == v)
 		return 0;
@@ -57,8 +45,7 @@ int query(int u, int v){
 	while(true){
 		uchain = chainidx[u];
 		if(uchain == vchain){
-			if(u == v)
-				break;
+			if(u == v) break;
 			//query on tree operation depends on the problem description
 			//querytree(1, 0, ptr, chainpos[v]+1, chainpos[u]+1);
 			break;
@@ -70,7 +57,6 @@ int query(int u, int v){
 	}
 	return ans;	
 }
-
 void solve(int root, int u, int v){
 	chainno = ptr = 0;
 	dfs(root, -1);
