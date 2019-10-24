72713fd3e3d21b44e9986b52f10f2e638ca1a547
Owmicron <55684780+Owmicron@users.noreply.github.com>
2019-10-24 20:48:47 +0800

    Update MinCostMaxFlow.cpp

--- MinCostMaxFlow.cpp
+++ MinCostMaxFlow.cpp
@@ -1,12 +1,6 @@
-struct Edge
-{
-    int from, to, capacity, cost;
-};
-
+struct Edge {int from, to, capacity, cost;};
 vector<vector<int>> adj, cost, capacity;
-
 const int INF = 1e9;
-
 void shortest_paths(int n, int v0, vector<int>& d, vector<int>& p) {
     d.assign(n, INF);
     d[v0] = 0;
@@ -14,7 +8,6 @@ void shortest_paths(int n, int v0, vector<int>& d, vector<int>& p) {
     deque<int> q;
     q.push_back(v0);
     p.assign(n, -1);
-
     while (!q.empty()) {
         int u = q.front();
         q.pop_front();
@@ -34,7 +27,6 @@ void shortest_paths(int n, int v0, vector<int>& d, vector<int>& p) {
         }
     }
 }
-
 int min_cost_flow(int N, vector<Edge> edges, int K, int s, int t) {
     adj.assign(N, vector<int>());
     cost.assign(N, vector<int>(N, 0));
@@ -46,7 +38,6 @@ int min_cost_flow(int N, vector<Edge> edges, int K, int s, int t) {
         cost[e.to][e.from] = -e.cost;
         capacity[e.from][e.to] = e.capacity;
     }
-
     int flow = 0;
     int cost = 0;
     vector<int> d, p;
@@ -54,7 +45,6 @@ int min_cost_flow(int N, vector<Edge> edges, int K, int s, int t) {
         shortest_paths(N, s, d, p);
         if (d[t] == INF)
             break;
-
         // find max flow on that path
         int f = K - flow;
         int cur = t;
@@ -62,7 +52,6 @@ int min_cost_flow(int N, vector<Edge> edges, int K, int s, int t) {
             f = min(f, capacity[p[cur]][cur]);
             cur = p[cur];
         }
-
         // apply flow
         flow += f;
         cost += f * d[t];
@@ -73,9 +62,6 @@ int min_cost_flow(int N, vector<Edge> edges, int K, int s, int t) {
             cur = p[cur];
         }
     }
-
-    if (flow < K)
-        return -1;
-    else
-        return cost;
+    if (flow < K) return -1;
+    else return cost;
 }
