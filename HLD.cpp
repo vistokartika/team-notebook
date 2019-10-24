e0641cd61ce16b1c8d6785e426d4a615cd7ffcb4
Owmicron <55684780+Owmicron@users.noreply.github.com>
2019-10-24 21:09:21 +0800

    Update HLD.cpp

--- HLD.cpp
+++ HLD.cpp
@@ -6,11 +6,8 @@ int ptr, chainno;
 void hld(int cur, int cost, int prev){
 	if(chainhead[chainno] == -1) chainhead[chainno] = cur;
 	chainidx[cur] = chainno;
-	pos[cur] = ptr;
-	basearr[ptr] = cost;
-	ptr++;
-	int nextcost, nextchild = -1;
-	int sz = adjlist[cur].size();
+	pos[cur] = ptr; basearr[ptr] = cost; ptr++;
+	int nextcost, nextchild = -1; int sz = adjlist[cur].size();
 	for(int i = 0; i < sz; i++){
 		if(adjlist[cur][i].first == prev) continue;
 		if(nextchild == -1 || subsize[nextchild] < subsize[adjlist[cur][i].first]){
@@ -21,13 +18,11 @@ void hld(int cur, int cost, int prev){
 	if(nextchild != -1) hld(nextchild, nextcost, cur);
 	for(int i = 0; i < sz; i++){
 		if(adjlist[cur][i].first == prev || adjlist[cur][i].first == nextchild) continue;
-		chainno++;
-		hld(adjlist[cur][i].first, adjlist[cur][i].second, cur);
+		chainno++; hld(adjlist[cur][i].first, adjlist[cur][i].second, cur);
 	}
 }
 void dfs(int cur, int prev){
-	subsize[cur] = 1;
-	int sz = adjlist[cur].size();
+	subsize[cur] = 1; int sz = adjlist[cur].size();
 	for(int i = 0; i < sz; i++){
 		if(adjlist[cur][i].first == prev)
 			continue;
@@ -37,11 +32,9 @@ void dfs(int cur, int prev){
 	}
 }
 int query(int u, int v){
-	if(u == v)
-		return 0;
+	if(u == v) return 0;
 	int uchain, vchain, ans;
-	vchain = chainidx[v];
-	ans = -1;
+	vchain = chainidx[v]; ans = -1;
 	while(true){
 		uchain = chainidx[u];
 		if(uchain == vchain){
@@ -52,14 +45,12 @@ int query(int u, int v){
 		}
 		//query on tree operation depends on the problem description
 		//querytree(1, 0, ptr, chainpos[chainhead[uchain]], chainpos[u]+1);
-		u = chainhead[uchain];
-		u = parent[u];
+		u = chainhead[uchain]; u = parent[u];
 	}
 	return ans;	
 }
 void solve(int root, int u, int v){
 	chainno = ptr = 0;
-	dfs(root, -1);
-	hld(root, -1, -1);
+	dfs(root, -1); hld(root, -1, -1);
 	int ans = query(u, v);
 }
