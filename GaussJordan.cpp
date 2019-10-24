edd3ae016e11e40844b70783774b0fbc4db2b760
Owmicron <55684780+Owmicron@users.noreply.github.com>
2019-10-24 20:45:06 +0800

    Update GaussJordan.cpp

--- GaussJordan.cpp
+++ GaussJordan.cpp
@@ -1,38 +1,24 @@
 // Gauss-Jordan elimination with full pivoting.
-//
 // Uses:
 //   (1) solving systems of linear equations (AX=B)
 //   (2) inverting matrices (AX=I)
 //   (3) computing determinants of square matrices
-//
 // Running time: O(n^3)
-//
 // INPUT:    a[][] = an nxn matrix
 //           b[][] = an nxm matrix
-//
 // OUTPUT:   X      = an nxm matrix (stored in b[][])
 //           A^{-1} = an nxn matrix (stored in a[][])
 //           returns determinant of a[][]
-
-#include <iostream>
-#include <vector>
-#include <cmath>
-
-using namespace std;
-
 const double EPS = 1e-10;
-
 typedef vector<int> VI;
 typedef double T;
 typedef vector<T> VT;
 typedef vector<VT> VVT;
-
 T GaussJordan(VVT &a, VVT &b) {
   const int n = a.size();
   const int m = b[0].size();
   VI irow(n), icol(n), ipiv(n);
   T det = 1;
-
   for (int i = 0; i < n; i++) {
     int pj = -1, pk = -1;
     for (int j = 0; j < n; j++) if (!ipiv[j])
@@ -43,9 +29,7 @@ T GaussJordan(VVT &a, VVT &b) {
     swap(a[pj], a[pk]);
     swap(b[pj], b[pk]);
     if (pj != pk) det *= -1;
-    irow[i] = pj;
-    icol[i] = pk;
-
+    irow[i] = pj; icol[i] = pk;
     T c = 1.0 / a[pk][pk];
     det *= a[pk][pk];
     a[pk][pk] = 1.0;
@@ -58,14 +42,11 @@ T GaussJordan(VVT &a, VVT &b) {
       for (int q = 0; q < m; q++) b[p][q] -= b[pk][q] * c;      
     }
   }
-
   for (int p = n-1; p >= 0; p--) if (irow[p] != icol[p]) {
     for (int k = 0; k < n; k++) swap(a[k][irow[p]], a[k][icol[p]]);
   }
-
   return det;
 }
-
 int main() {
   const int n = 4;
   const int m = 2;
@@ -76,12 +57,9 @@ int main() {
     a[i] = VT(A[i], A[i] + n);
     b[i] = VT(B[i], B[i] + m);
   }
-  
   double det = GaussJordan(a, b);
-  
   // expected: 60  
   cout << "Determinant: " << det << endl;
-
   // expected: -0.233333 0.166667 0.133333 0.0666667
   //           0.166667 0.166667 0.333333 -0.333333
   //           0.233333 0.833333 -0.133333 -0.0666667
@@ -92,7 +70,6 @@ int main() {
       cout << a[i][j] << ' ';
     cout << endl;
   }
-  
   // expected: 1.63333 1.3
   //           -0.166667 0.5
   //           2.36667 1.7
