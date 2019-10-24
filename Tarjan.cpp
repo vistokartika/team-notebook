a7c907710da1bf668cd91cd301d97bf7606dcc3c
vistokartika <38373457+vistokartika@users.noreply.github.com>
2019-10-24 19:32:52 +0700

    Update Tarjan.cpp

--- Tarjan.cpp
+++ Tarjan.cpp
@@ -1,109 +1,5 @@
-#include<bits/stdc++.h>
-
-using namespace std;
-
-int n,m,u,v,d,num[105],low[105],par[105],child,arr[105],root,cnt;
-vector<int> adjlist[105];
-bool flag;
-
-void dfs(int now)
-{
-	num[now]=d;
-	low[now]=d;
-	d++;
-	for(int i=0;i<adjlist[now].size();i++)
-	{
-		int next=adjlist[now][i];
-		if(num[next]==-1)
-		{
-			par[next]=now;
-			dfs(next);
-			if(root==now)
-			{
-				child++;
-			}
-			if(low[next]>=num[now])
-			{
-				arr[now]=1;
-			}
-			low[now]=min(low[now],low[next]);
-		}
-		else if(next!=par[now])
-		{
-			low[now]=min(low[now],num[next]);
-		}
-	}
-}
-
-int main()
-{
-	ios_base::sync_with_stdio(false);
-    cin.tie(NULL); cout.tie(NULL);
-    while(cin>>n)
-    {
-    	d=1;
-    	cnt=0;
-    	memset(arr,0,sizeof(arr));
-    	memset(par,-1,sizeof(par));
-		memset(num,-1,sizeof(num));
-		memset(low,-1,sizeof(low));
-		for(int i=1;i<=n;i++)
-		{
-			adjlist[i].clear();
-		}
-		
-    	if(n==0)
-    	{
-    		return 0;
-		}
-    	string s;
-        while (getline(cin, s))
-        {
-        	if(s=="0")
-        	{
-				break;
-			}
-            stringstream ss(s);
-            int u, v;
-            ss >> u;
-            while (ss >> v)
-            {
-                adjlist[u].push_back(v);
-                adjlist[v].push_back(u);
-            }
-        }
-		for(int i=1;i<=n;i++)
-		{
-			if(num[i]==-1)
-			{
-				root=i;
-				child=0;
-				dfs(i);
-				if(child>1)
-				{
-					arr[i]=1;
-				}
-				else
-				{
-					arr[i]=0;
-				}
-			}
-		}
-		for(int i=1;i<=n;i++)
-		{
-			if(arr[i]>0)
-			{
-				cnt++;
-			}
-		}
-		cout<<cnt<<endl;
-    }
-}
-
-//Finding articulation points and bridges in a general graph
-
 const int GraphSize = 1000;
-int root, child, counter;
+int root, child, counter, n, m, u, v;
 int num[GraphSize], low[GraphSize], parent[GraphSize];
 bool artpoint[GraphSize];
 vector<int> adjlist[GraphSize];
@@ -135,18 +31,33 @@ void tarjan(int front){
 	}
 }
 
-void solve(int v){
-	//v is the number of vertices
+void solve(int n){
+	//n is the number of vertices
 	counter = 0;
 	memset(num, -1, sizeof(num));
 	memset(low, 0, sizeof(low));
-	for(int i = 0; i < v; i++){
+	for(int i = 0; i < n; i++){
 		if(num[i] == -1){
 			root = i;
 			child = 0;
 			tarjan(i);
 			if(child > 1)
 				artpoint[i] = true;
+		}
+	}
+}
+
+int main() {
+	cin >> n >> m;
+	for(int i = 0; i < m; i++) {
+		cin >> u >> v;
+		adjlist[u].push_back(v);
+		adjlist[v].push_back(u);
+	}
+	solve(n);
+	for(int i = 0; i < n; i++) {
+		if(artpoint[i] == true) {
+			cout << i << " is articulation point" << endl;
 		}
 	}
 }
