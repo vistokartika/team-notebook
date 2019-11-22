d5c8722aeadc47ba3385556eb763a89fd51ef8c3
vistokartika <38373457+vistokartika@users.noreply.github.com>
2019-11-22 13:22:55 +0700

    Update Mo.cpp

--- Mo.cpp
+++ Mo.cpp
@@ -3,62 +3,68 @@
 using namespace std;
 
 struct qwer {
-    long long l,r,idx;
+    int l,r,idx;
 };
 
-long long n,q,arr[200005],cnt[1000005],m,le,ri,ret[200005],ans=0;
+int n,q,arr[30005],cnt[1000005],m,le,ri,ret[200005],ans=0;
 qwer qq[200005];
 
 bool compare(qwer a,qwer b) {
     if((a.l/m)!=(b.l/m))
         return (a.l/m)<(b.l/m);
-    else
-    	return a.r<b.r;
+    if((a.l/m)&1)
+    	return a.r>b.r;
+    return a.r<b.r;
 }
 
 int main() {
-	cin>>n>>q;
+	cin>>n;
 	m=sqrt(n);
-	for(long long i=0;i<n;i++) {
-		scanf("%I64d",&arr[i]);
+	for(int i=0;i<n;i++) {
+		scanf("%d",&arr[i]);
 	}
-	for(long long i=0;i<q;i++) {
-		scanf("%I64d %I64d",&le,&ri);
+	cin>>q;
+	for(int i=0;i<q;i++) {
+		cin>>le>>ri;
 		qq[i].l=le-1;
 		qq[i].r=ri-1;
 		qq[i].idx=i;
 	}
-	stable_sort(qq,qq+q,compare);
-	long long mole=0, mori=-1;
-	for(long long i=0;i<q;i++) {
-		long long left=qq[i].l, right=qq[i].r;
+	sort(qq,qq+q,compare);
+	int mole=0, mori=-1;
+	for(int i=0;i<q;i++) {
+		int left=qq[i].l, right=qq[i].r;
 		while(mori<right) {
 			mori++;
-			ans-=(cnt[arr[mori]]*cnt[arr[mori]]*arr[mori]);
+			if(cnt[arr[mori]]==0) {
+				ans++;
+			}
 			cnt[arr[mori]]++;
-			ans+=(cnt[arr[mori]]*cnt[arr[mori]]*arr[mori]);
 		}
 		while(mori>right) {
-			ans-=(cnt[arr[mori]]*cnt[arr[mori]]*arr[mori]);
 			cnt[arr[mori]]--;
-			ans+=(cnt[arr[mori]]*cnt[arr[mori]]*arr[mori]);
+			if(cnt[arr[mori]]==0) {
+				ans--;
+			}
 			mori--;
 		}
 		while(mole>left) {
 			mole--;
-			ans-=(cnt[arr[mole]]*cnt[arr[mole]]*arr[mole]);
+			if(cnt[arr[mole]]==0) {
+				ans++;
+			}
 			cnt[arr[mole]]++;
-			ans+=(cnt[arr[mole]]*cnt[arr[mole]]*arr[mole]);
 		}
 		while(mole<left) {
-			ans-=(cnt[arr[mole]]*cnt[arr[mole]]*arr[mole]);
 			cnt[arr[mole]]--;
-			ans+=(cnt[arr[mole]]*cnt[arr[mole]]*arr[mole]);
+			if(cnt[arr[mole]]==0) {
+				ans--;
+			}
 			mole++;
 		}
 		ret[qq[i].idx]=ans;
 	}
-	for(long long i=0;i<q;i++) {
-		printf("%I64d\n",ret[i]);
+	for(int i=0;i<q;i++) {
+		printf("%d\n",ret[i]);
 	}
 }
