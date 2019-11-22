e0c8be29b31c68c9b009ec6046a5a5880c40cbc4
vistokartika <38373457+vistokartika@users.noreply.github.com>
2019-11-22 12:58:53 +0700

    Update SegmentTree.cpp

--- SegmentTree.cpp
+++ SegmentTree.cpp
@@ -1,9 +1,30 @@
-//Segment Tree + Lazy Propagation for RMQ with additional updates
+//Topic: Segment Tree + Lazy Propagation
+//Query: Sum of range
+//Updates: addition
 
 #include <bits/stdc++.h>
 using namespace std;
 
-long long n, a[200010], q, tree[4 * 200010], lazy[4 * 200010], inf = 1e18 + 10, lf, rg, v;
+int n, a[100010], q, tree[4 * 100010], lazy[4 * 100010], lf, rg, v, type;
+
+void init(int now, int l, int r, int val) {
+	tree[now] += (r - l + 1) * val;
+    if(l != r){
+        lazy[now * 2] += val;
+        lazy[now * 2 + 1] += val;
+    }
+}
+
+void drop_down(int now, int l, int r) {
+	if(lazy[now] != 0) {
+		tree[now] += (r - l + 1) * lazy[now];
+        if(l!=r) {
+            lazy[now * 2] += lazy[now];
+            lazy[now * 2 + 1] += lazy[now];
+        }
+        lazy[now] = 0;
+	}
+}
 
 void build(int now, int l, int r) {
 	if(l == r) {
@@ -13,88 +34,55 @@ void build(int now, int l, int r) {
 	int m = (l + r) / 2;
 	build(now * 2, l, m);
 	build(now * 2 + 1, m + 1, r);
-	tree[now] = min(tree[now * 2], tree[now * 2 + 1]);
+	tree[now] = tree[now * 2] + tree[now * 2 + 1];
 }
 
 void update(int now, int l, int r, int st, int en, int val) {
-	// drop down
-	if(lazy[now] != 0) {
-		tree[now] += lazy[now];
-        if(l!=r) {
-            lazy[now * 2] += lazy[now];
-            lazy[now * 2 + 1] += lazy[now];
-        }
-        lazy[now] = 0;
-	}
+	drop_down(now, l, r);
 	if(en < l || r < st) {
 		return;
 	}
-	//initialize drop down
 	if(st <= l && r <= en){
-        tree[now] += val;
-        if(l != r){
-            lazy[now * 2] += val;
-            lazy[now * 2 + 1] += val;
-        }
+        init(now, l, r ,val);
         return;
     }
     int m = (l + r) / 2;
     update(now * 2, l, m, st, en, val);
     update(now * 2 + 1, m + 1, r, st, en, val);
-    tree[now] = min(tree[now * 2], tree[now * 2 + 1]);
+    tree[now] = tree[now * 2] + tree[now * 2 + 1];
 }
 
-long long query(int now, int l, int r, int st, int en) {
+int query(int now, int l, int r, int st, int en) {
 	if(en < l || r < st) {
-		return inf;
+		return 0;
 	}
-	//drop down
-	if(lazy[now] != 0) {
-		tree[now] += lazy[now];
-        if(l!=r) {
-            lazy[now * 2] += lazy[now];
-            lazy[now * 2 + 1] += lazy[now];
-        }
-        lazy[now] = 0;
-	}
+	drop_down(now, l, r);
 	if(st <= l && r <= en) {
 		return tree[now];
 	}
 	int m = (l + r) / 2;
-    return min(query(now * 2, l, m, st, en), query(now * 2 + 1, m + 1, r, st, en));
+	int ret = query(now * 2, l, m, st, en) + query(now * 2 + 1, m + 1, r, st, en);
+    return ret;
 }
 
 int main() {
 	ios_base :: sync_with_stdio(false);
 	cin.tie(NULL); cout.tie(NULL);
 	memset(lazy, 0, sizeof(lazy));
-	cin >> n;
+	cin >> n >> q;
 	for(int i = 1; i <= n; i++) {
 		cin >> a[i];
 	}
 	build(1, 1, n);
-	cin >> q;
 	for(int i = 1; i <= q; i++) {
-		cin >> lf >> rg;
-		lf++;
-		rg++;
-		if(cin.get() == 10) {
-			if(lf <= rg) {
-				cout << query(1, 1, n, lf, rg) << endl;
-			}
-			else {
-				cout << min(query(1, 1, n, lf, n), query(1, 1, n, 1, rg)) << endl;
-			}
+		cin >> type;
+		if(type == 0) { //update
+			cin >> lf >> rg >> v;
+			update(1, 1, n, lf, rg, v);
 		}
-		else {
-			cin >> v;
-			if(lf <= rg) {
-				update(1, 1, n, lf, rg, v);
-			}
-			else {
-				update(1, 1, n, lf, n, v);
-				update(1, 1, n, 1, rg, v);
-			}
+		else { //query
+			cin >> lf >> rg;
+			cout << query(1, 1, n, lf, rg) << endl;
 		}
 	}
 	return 0;
